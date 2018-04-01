eeebc11d87ed1d8121104ed2970378c63e925c23
Lihkin <shniks@gmail.com>
2018-04-01 16:59:54 -0600

    Created passing test for idea create action.

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -4,4 +4,19 @@ class IdeasController < ApplicationController
     @idea = Idea.new
   end
 
+  def create
+    @idea = Idea.new(idea_params)
+    @idea.save
+    redirect_to idea_path(@idea)
+  end
+
+  def show
+    @idea = Idea.find(params[:id])
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :description)
+  end
 end
--- spec/features/ideas/user_can_create_an_idea_spec.rb
+++ spec/features/ideas/user_can_create_an_idea_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'User visits the /ideas/new page' do
   describe 'They can fill in details for an idea and click submit' do
-    scenario 'They can see that idea\'s details on the idea show page' do
+    scenario 'They can see that idea\'s details on its show page' do
 
       visit new_idea_path
 
@@ -11,7 +11,7 @@ describe 'User visits the /ideas/new page' do
 
       click_on 'Submit Idea'
 
-      expect(current_path).to eq(ideas_path)
+      expect(current_path).to eq('/ideas/1')
       expect(page).to have_content('Sample Idea')
       expect(page).to have_content('Sample Description')
     end

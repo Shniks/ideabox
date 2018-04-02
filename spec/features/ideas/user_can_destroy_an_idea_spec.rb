6ad56a53cce46016329ecd7c2e0ea0a2465c213a
Lihkin <shniks@gmail.com>
2018-04-01 19:48:26 -0600

    Added feature test to delete an idea.

---
+++ spec/features/ideas/user_can_destroy_an_idea_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'User visits ideas index' do
+  describe 'They click the delete button next to an idea' do
+    scenario 'The idea is removed from the ideas index' do
+
+      idea = Idea.create!(title: 'Save Photos', description: 'Save Photos Description')
+
+      visit ideas_path
+
+      within(".idea_#{idea.id}") do
+        click_link 'Delete'
+      end
+
+      expect(page).to have_content("#{idea.title} delete successfully!")
+      expect(page).to_not have_content(idea.title)
+      expect(page).to_not have_content(idea.description)
+    end
+  end
+end

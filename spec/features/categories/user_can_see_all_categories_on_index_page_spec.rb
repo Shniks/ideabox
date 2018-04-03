b47afdc4336a67bafa92763c63a1e0cf2bf2f9fc
Lihkin <shniks@gmail.com>
2018-04-02 21:51:38 -0600

    Added feature test for categories index page.

---
+++ spec/features/categories/user_can_see_all_categories_on_index_page_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'User visits the categories index' do
+  scenario 'They can see a list of all the categories' do
+
+    category_1 = Category.create!(name: 'Photography')
+    category_2 = Category.create!(name: 'Knitting')
+    category_3 = Category.create!(name: 'Cars')
+    category_4 = Category.create!(name: 'Sports')
+
+    visit categories_path
+
+    expect(page).to have_content('Photography')
+    expect(page).to have_content('Knitting')
+    expect(page).to have_content('Cars')
+    expect(page).to have_content('Sports')
+  end
+end

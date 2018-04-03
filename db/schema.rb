78b5bc24ed584abc20d7fe60716e2c6e162c48a0
Lihkin <shniks@gmail.com>
2018-04-02 22:14:06 -0600

    Created one to many relationship between ideas and categories. Added and ran migration for the foreign key.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403031645) do
+ActiveRecord::Schema.define(version: 20180403041030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20180403031645) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_ideas_on_category_id"
   end
 
+  add_foreign_key "ideas", "categories"
 end
--- spec/features/ideas/user_can_create_an_idea_spec.rb
+++ spec/features/ideas/user_can_create_an_idea_spec.rb
@@ -4,7 +4,11 @@ describe 'User visits the new idea path to create a new idea' do
   describe 'They can fill in details for an idea and click submit' do
     scenario 'They can see that new idea\'s details on the ideas index page' do
 
+      category = Category.create!(name: 'Photography')
+
       visit new_idea_path
+
+      save_and_open_page
 
       fill_in 'idea[title]', with: 'Sample Idea'
       fill_in 'idea[description]', with: 'Sample Description'

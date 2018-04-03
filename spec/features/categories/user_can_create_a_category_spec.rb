528325c6e89a4f864cb30154165fd829a0b5d9bf
Lihkin <shniks@gmail.com>
2018-04-02 21:06:38 -0600

    Created feature test to create a new category.

---
+++ spec/features/categories/user_can_create_a_category_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'User visits a new category path to create a new category' do
+  describe 'They can fill in the details for a new category and click submit' do
+    scenario 'They can see the new category\'s details on the categories index page' do
+
+      visit new_category_path
+
+      fill_in 'category[name]' with: 'Sample Category'
+
+      click_on 'Create Category'
+
+      expect(path).to eq(categories_path)
+      expect(page).to have_content('Sample Category')
+    end
+  end
+end

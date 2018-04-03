0a51d99974d12af2489bb108ae3d07ab89cfb41c
Lihkin <shniks@gmail.com>
2018-04-02 23:08:47 -0600

    Added relationship model test for idea.

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,12 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe Idea, type: :model do
+describe Idea do
   describe 'Validations' do
-    it { validate_presence_of :title }
-    it { validate_presence_of :body }
+    context 'Invalid attributes' do
+      it 'Is invalid without a title' do
+        idea = Idea.new
+        expect(idea).to be_invalid
+      end
+
+      it 'Is invalid with only a title' do
+        idea = Idea.new(title: 'Invalid Idea')
+        expect(idea).to be_invalid
+      end
+
+      it 'Is invalid with only a description' do
+        idea = Idea.new(description: 'Invalid Idea')
+        expect(idea).to be_invalid
+      end
+
+      it 'Has a unique title' do
+        Idea.create(title: 'Title1', description: 'Great Idea')
+        idea = Idea.create(title: 'Title1', description: 'Great Idea')
+        expect(idea).to be_invalid
+      end
+
+      it 'Has a unique description' do
+        Idea.create(title: 'Title1', description: 'Great Idea')
+        idea = Idea.create(title: 'Title2', description: 'Great Idea')
+        expect(idea).to be_invalid
+      end
+
+      it 'Has a unique title and description' do
+        category_1 = Category.create!(name: 'Photography')
+        Idea.create(title: 'Title1', description: 'Great Idea', category: category_1)
+        idea = Idea.create(title: 'Title2', description: 'Great Idea 2', category: category_1)
+        expect(idea).to be_valid
+      end
+    end
+
+    context 'Valid attributes' do
+      it 'Is valid with a title and a description' do
+        category_1 = Category.create!(name: 'Photography')
+        idea = Idea.new(title: 'Idea 1 Idea', description: 'Idea 1 Description',  category: category_1)
+        expect(idea).to be_valid
+      end
+    end
   end
 
   describe 'Relationships' do
-    it { should belong_to(:category) }
+    scenario { should belong_to(:category) }
   end
 end

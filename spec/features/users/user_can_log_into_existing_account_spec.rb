8d5167fb6ece479b2f57e03725b9b180244ef779
Lihkin <shniks@gmail.com>
2018-04-03 15:57:01 -0600

    Added feature test for existing user to log in.

---
+++ spec/features/users/user_can_log_into_existing_account_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'User visits the home page' do
+  describe 'They click on I already have an account' do
+    describe 'They get taken to a login page where they fill their details and click on Log In' do
+      scenario 'They are logged in and taken to the welcome page' do
+
+        user = User.create(username: "funbucket13", password: "test")
+
+        visit '/'
+        click_on 'I already have an account'
+
+        expect(current_path).to eq(login_path)
+
+        fill_in 'username', with: user.username
+        fill_in 'password', with: user.password
+
+        click_on 'Log In'
+
+        expect(current_path).to eq(user_path(user))
+        expect(page).to have_content("Welcome, #{user.username}!")
+        expect(page).to have_content('Logout')
+      end
+    end
+  end
+end

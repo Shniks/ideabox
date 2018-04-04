cebfb4d438f0d7c557c7317ea8a7b7d1489f6f24
Lihkin <shniks@gmail.com>
2018-04-03 23:35:58 -0600

    Added migration and ran the migration to add role to users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404003311) do
+ActiveRecord::Schema.define(version: 20180404053428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20180404003311) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0
   end
 
   add_foreign_key "hyperlinks", "ideas"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,4 +15,20 @@ describe User, type: :model do
       expect(copy_cat).to_not be_valid
     end
   end
+
+  describe "Roles" do
+    it "Can be created as an admin" do
+      user = User.create(username: "penelope", password: "boom", role: 1)
+
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_truthy
+    end
+
+    it "Can be created as a default user" do
+      user = User.create(username: "sammy", password: "pass", role: 0)
+
+      expect(user.role).to eq("default")
+      expect(user.default?).to be_truthy
+    end
+  end
 end
